d1491fb105362cbd952373899bc771e67461f945
Fractal-Tess <vgfractal@gmail.com>
2022-07-30 02:11:16 +0300

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '$styles';
+	import '@fortawesome/fontawesome-free/js/all.js';
+	import { fade } from 'svelte/transition';
+
+	import Header from '$lib/example/header/Header.svelte';
+	import Footer from '$lib/example/footer/Footer.svelte';
+
+	import { theme } from '$lib/stores/theme';
+	$: {
+		document.documentElement.setAttribute('data-theme', $theme);
+		document.documentElement.classList.value = $theme;
+	}
+</script>
+
+{#await theme.load() then}
+	<div
+		class="bg-base-100 text-base-content h-screen flex flex-col overflow-y-auto overflow-x-hidden"
+	>
+		<Header />
+		<main class="flex-1" in:fade={{ delay: 300, duration: 1000 }}>
+			<slot />
+		</main>
+		<Footer />
+	</div>
+{/await}
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { join } from 'node:path';
+
+/** @type {import('vite').UserConfig} */
+
+const config = {
+	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "src/styles/variables.scss" as *;'
+			}
+		}
+	},
+
+	resolve: {
+		alias: {
+			$styles: join(__dirname, 'src/styles/app.scss')
+		}
+	},
+	build: {
+		reportCompressedSize: false
+	},
+	server: {
+		port: +(process.env.PORT || 3000),
+		host: '127.0.0.1',
+		strictPort: true
+	}
+};
+
+export default config;
