2eb85b2f0768d74d2c9235b13047196409b195e1
Fractal-Tess <vgfractal@gmail.com>
2022-09-12 10:11:58 +0300

    reverted ot stock daisyui themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,30 +19,10 @@ const config = {
   daisyui: {
     themes: [
       {
-        light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
-          primary: '#e863d6',
-          secondary: '#7bc3e0',
-          accent: '#cd97fc',
-          neutral: '#25343C',
-          'base-100': '#F8F8FB',
-          info: '#93BADC',
-          success: '#12A168',
-          warning: '#F4C857',
-          error: '#DE3B4E'
-        },
-        dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
-          primary: '#137cb5',
-          secondary: '#e80d52',
-          accent: '#f4cd73',
-          neutral: '#272727',
-          'base-100': '#1e1e1e',
-          info: '#35B0F3',
-          success: '#1B743C',
-          warning: '#F38A12',
-          error: '#F2215C'
-        }
+        // Expand this as peer needs or color theme
+        // See https://daisyui.com/docs/themes/ & https://daisyui.com/theme-generator/
+        light: require('daisyui/src/colors/themes')['[data-theme=light]'],
+        dark: require('daisyui/src/colors/themes')['[data-theme=dark]']
       }
     ]
   },
